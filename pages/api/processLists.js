683346f394431bb83f7f0b1f0eb0d09b32b69539
Teale Fristoe <fristoe@gmail.com>
2020-12-08 20:37:45 -0800

    Reduce photo data stored and update everyone's lists.

--- pages/api/processLists.js
+++ pages/api/processLists.js
@@ -1,13 +1,19 @@
 
 import {replaceSpaces, getParams} from '../../lib/web'
-import {processData, writeProcessedList} from '../../lib/data'
+import {getBirders, processData, writeProcessedList} from '../../lib/data'
+
+function process(birder) {
+  writeProcessedList(birder, processData(birder))
+}
 
 // an api end point for processing raw life and photo lists for a user
 export default (req, res) => {
   let {user} = getParams(req.url)
   user = replaceSpaces(user)
-  let processedList = processData(replaceSpaces(user))
-  writeProcessedList(user, processedList)
-  //res.status(200).json(processData(replaceSpaces(user)))
+  if (user == 'all') {
+    getBirders().forEach(birder => process(birder))
+  } else {
+    process(user)
+  }
   res.status(200).end()
 }
